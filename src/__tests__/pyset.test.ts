e45a7c43a896a993557229e28ebcfb14c9253ceb
amardeep24 <amardeep.bhowmick92@gmail.com>
2020-12-12 22:51:56 +0530

    Test cases written

--- src/__tests__/pyset.test.ts
+++ src/__tests__/pyset.test.ts
@@ -1,10 +1,10 @@
 import PySet from "../pyset";
 
-describe('PySet Operations should work as expected', () => {
-    let pyset;
+describe('PySet Operations with one set', () => {
+    let pyset: PySet<string>;
 
     beforeEach(() => {
-        pyset = new PySet();
+        pyset = new PySet<string>();
     });
 
     test("discard", () => {
@@ -14,54 +14,148 @@ describe('PySet Operations should work as expected', () => {
     });
 
     test("testCopy", () => {
-
+        pyset.add("foo");
+        const copiedSet = pyset.copy();
+        expect(Array.from(copiedSet).every(s => pyset.has(s))).toEqual(true);
     });
 
     test("pop", () => {
+        pyset.add("foo");
+        const val = pyset.pop();
+        expect(val).toEqual("foo");
+    });
+});
 
+describe('PySet Operations with two sets', () => {
+
+    let pysetOne: PySet<string>;
+    let pysetTwo: PySet<string>;
+    let pysetThree: PySet<string>;
+    let pysetFour: PySet<string>;
+
+    beforeEach(() => {
+        pysetOne = new PySet<string>();
+        pysetTwo = new PySet<string>();
+
+        pysetThree = new PySet<string>();
+        pysetFour = new PySet<string>();
     });
 
     test("testIsDisjoint", () => {
+        pysetOne.add("foo").add("fuu");
+        pysetTwo.add("baz").add("bar");
 
+        const resOne = pysetOne.isDisjoint(pysetTwo);
+        expect(resOne).toEqual(true);
+
+        pysetThree.add("foo").add("bar");
+        pysetFour.add("baz").add("bar");
+
+        const resTwo = pysetThree.isDisjoint(pysetFour);
+        expect(resTwo).toEqual(false);
     });
 
     test("testIsSubset", () => {
+        pysetOne.add("foo").add("bar").add("baz");
+        pysetTwo.add("bar").add("foo");
 
+        const resOne = pysetTwo.isSubset(pysetOne);
+        expect(resOne).toEqual(true);
+
+        pysetThree.add("fuu");
+
+        const resTwo = pysetThree.isSubset(pysetOne);
+        expect(resTwo).toEqual(false);
     });
 
     test("testIsSuperset", () => {
+        pysetOne.add("foo").add("bar").add("baz");
+        pysetTwo.add("bar").add("foo");
 
+        const resOne = pysetOne.isSuperset(pysetTwo);
+        expect(resOne).toEqual(true);
+
+        pysetThree.add("fuu");
+
+        const resTwo = pysetOne.isSuperset(pysetThree);
+        expect(resTwo).toEqual(false);
     });
 
     test("testUnion", () => {
+        pysetOne.add("foo").add("bar");
+        pysetTwo.add("baz");
 
+        const resOne = pysetOne.union(pysetTwo);
+        expect([...resOne]).toEqual(["foo", "bar", "baz"]);
+
+        pysetThree.add("fuu");
+        const resTwo = pysetOne.union(pysetTwo, pysetThree);
+        expect([...resTwo]).toEqual(["foo", "bar", "baz", "fuu"]);
     });
 
     test("testIntersection", () => {
+        pysetOne.add("foo").add("bar");
+        pysetTwo.add("bar");
 
+        const resOne = pysetOne.intersection(pysetTwo);
+        expect([...resOne]).toEqual(["bar"]);
+
+        pysetThree.add("foo")
+        const resTwo = pysetOne.intersection(pysetTwo, pysetThree);
+        expect([...resTwo]).toEqual(["bar", "foo"]);
     });
 
     test("testDifference", () => {
+        pysetOne.add("foo").add("bar");
+        pysetTwo.add("bar");
 
+        const resOne = pysetOne.difference(pysetTwo);
+        pysetThree.add("foo")
+        expect(resOne).toEqual(pysetThree);
     });
 
     test("testSymmetricDifference", () => {
+        pysetOne.add("foo").add("bar");
+        pysetTwo.add("bar").add("baz");
 
+        const resOne = pysetOne.symmetricDifference(pysetTwo);
+        pysetThree.add("foo").add("baz");
+        expect(resOne).toEqual(pysetThree);
     });
 
     test("testUpdate", () => {
+        pysetOne.add("foo").add("bar");
+        pysetTwo.add("baz");
 
+        const resOne = pysetOne.update(pysetTwo);
+        pysetThree.add("foo").add("bar").add("baz");
+        expect(resOne).toEqual(pysetThree);
     });
 
     test("testIntersectionUpdate", () => {
+        pysetOne.add("foo").add("bar");
+        pysetTwo.add("bar");
 
+        const resOne = pysetOne.intersectionUpdate(pysetTwo);
+        pysetThree.add("bar");
+        expect(resOne).toEqual(pysetThree);
     });
 
     test("testDifferenceUpdate", () => {
+        pysetOne.add("foo").add("bar").add("baz");
+        pysetTwo.add("bar");
 
+        const resOne = pysetOne.differenceUpdate(pysetTwo);
+        pysetThree.add("baz").add("foo");;
+        expect(resOne).toEqual(pysetThree);
     });
 
     test("testSymmetricDifferenceUpdate", () => {
+        pysetOne.add("foo").add("bar");
+        pysetTwo.add("baz").add("bar");
 
+        const resOne = pysetOne.symmetricDifferenceUpdate(pysetTwo);
+        pysetThree.add("baz").add("foo");;
+        expect(resOne).toEqual(pysetThree);
     });
 });
--- src/pyset.ts
+++ src/pyset.ts
@@ -1,58 +1,147 @@
 export default class PySet<T> extends Set<T>{
 
-    public discard(val: T){
+    /**
+     *  Remove an element from a set if it is a member.
+     *  If the element is not a member, do nothing.
+     * @param val 
+     */
+    public discard(val: T) {
 
     }
 
-    public copy(val: T){
-        return new Set();
+    /**
+     *  Return a shallow copy of a set.
+     */
+    public copy(): Set<T> {
+        return new Set(this);
     }
 
-    public pop(val: T): T{
+    /**
+     * Remove and return an arbitrary set element.
+     * Raises KeyError if the set is empty.
+     */
+    public pop(): T {
         return null;
     }
 
-    public isDisjoint(other: Set<T>): boolean{
-        return false;
+    /**
+     * Return True if two sets have a null intersection.
+     * @param other 
+     */
+    public isDisjoint(other: Set<T>): boolean {
+        for (const e of other) {
+            if (this.has(e)) {
+                return false;
+            }
+        }
+        return true;
     }
-    
-    public isSubset(other: Set<T>): boolean{
-        return false;
+
+    /**
+     * Report whether another set contains this set.
+     * @param other 
+     */
+    public isSubset(other: Set<T>): boolean {
+        for (const e of this) {
+            if (!other.has(e)) {
+                return false;
+            }
+        }
+        return true;
     }
-    
-    public isSuperset(other: Set<T>): boolean{
-        return false;
+
+    /**
+     * Report whether this set contains another set.
+     * @param other 
+     */
+    public isSuperset(other: Set<T>): boolean {
+        for (const e of other) {
+            if (!this.has(e)) {
+                return false;
+            }
+        }
+        return true;
     }
-    
-    public union(...others: Array<Set<T>>){
+
+    /**
+     * Return the union of sets as a new set.
+     * (i.e. all elements that are in either set.)
+     * @param others 
+     */
+    public union(...others: Array<Set<T>>) {
+        const result = new Set(this);
+        for (const set of others){
+            for(const e of set){
+                result.add(e);
+            }
+        }
+        return result;
+    }
+
+    /**
+     *  Return the intersection of two sets as a new set.
+     *  (i.e. all elements that are in both sets.)
+     * @param others 
+     */
+    public intersection(...others: Array<Set<T>>): Set<T> {
+        const result = new Set<T>();
+        for (const set of others){
+            for(const e of set){
+                if(this.has(e)){
+                    result.add(e);
+                }
+            }
+        }
+        return result;
+    }
+
+    /**
+     *  Return the difference of two or more sets as a new set.
+     *  (i.e. all elements that are in this set but not the others.)
+     * @param others 
+     */
+    public difference(...others: Array<Set<T>>) {
         return new Set();
     }
-    
-    public intersection(...others: Array<Set<T>>): Set<T>{
+
+    /**
+     *  Return the symmetric difference of two sets as a new set.
+     *  (i.e. all elements that are in exactly one of the sets.)
+     * @param others 
+     */
+    public symmetricDifference(other: Set<T>) {
         return new Set();
     }
-    
-    public difference(...others: Array<Set<T>>){
+
+    /**
+     * Update a set with the union of itself and others.
+     * @param others 
+     */
+    public update(...others: Array<Set<T>>) {
         return new Set();
     }
-    
-    public symmetricDifference(other: Set<T>){
+
+    /**
+     * Update a set with the intersection of itself and another.
+     * @param others 
+     */
+    public intersectionUpdate(...others: Array<Set<T>>) {
         return new Set();
     }
-    
-    public update(...others: Array<Set<T>>){
+
+    /**
+     * Remove all elements of another set from this set.
+     * @param others 
+     */
+    public differenceUpdate(...others: Array<Set<T>>) {
         return new Set();
     }
-    
-    public intersectionUpdate(...others: Array<Set<T>>){
-        return new Set();
-    }
-    
-    public differenceUpdate(...others: Array<Set<T>>){
-        return new Set();
-    }
-    
-    public symmetricDifferenceUpdate(...others: Array<Set<T>>){
+
+    /**
+     * Update a set with the symmetric difference of itself and another.
+     * @param others 
+     */
+    public symmetricDifferenceUpdate(...others: Array<Set<T>>) {
         return new Set();
     }
 }
